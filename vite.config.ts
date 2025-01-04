6285b94a646f74b6c3264be65cd85d0d18f28686
Oleksandr Bystrikov <coder@yakviter.com.ua>
2025-01-04 01:40:27 +0100

    refactor: upgrade vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,22 @@
-import vue from '@vitejs/plugin-vue'
-import * as path from 'path'
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
 
 export default defineConfig({
   build: {
     lib: {
-      name: 'vue-stripe-js',
-      entry: path.resolve(__dirname, 'src/main.ts'),
+      name: "vue-stripe-js",
+      entry: "src/main.ts",
       fileName: (format) => `vue-stripe.${format}.js`,
     },
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: ['vue'],
+      external: ["vue"],
       output: {
         // Provide global variables to use in the UMD build
         // for externalized deps
         globals: {
-          vue: 'Vue',
+          vue: "Vue",
         },
       },
     },
