2a6b793f55eb85804ae025fe87d580b6e10e3439
Oleksandr Bystrikov <coder@yakviter.com.ua>
2025-01-06 01:33:37 +0100

    fix(formatting): use biome

--- tests/unit/stripe-elements.spec.js
+++ tests/unit/stripe-elements.spec.js
@@ -1,18 +1,18 @@
 import {
-  initStripe,
-  createElements,
-  createElement,
   ERRORS,
-} from '../../src/stripe-elements'
+  createElement,
+  createElements,
+  initStripe,
+} from "../../src/stripe-elements"
 
-describe('initStripe', () => {
-  test('no key, no options', () => {
+describe("initStripe", () => {
+  test("no key, no options", () => {
     const result = initStripe()
     expect(result.message).toBe(ERRORS.STRIPE_NOT_LOADED)
   })
 
-  test('key, no options', () => {
-    const key = '12345'
+  test("key, no options", () => {
+    const key = "12345"
     const mockFn = jest.fn()
     window.Stripe = mockFn
     initStripe(key)
@@ -20,9 +20,9 @@ describe('initStripe', () => {
     expect(mockFn).toHaveBeenCalledWith(key, undefined)
   })
 
-  test('key and options', () => {
-    const key = '12345'
-    const options = { c: 'd' }
+  test("key and options", () => {
+    const key = "12345"
+    const options = { c: "d" }
     const mockFn = jest.fn()
     window.Stripe = mockFn
     initStripe(key, options)
@@ -31,54 +31,54 @@ describe('initStripe', () => {
   })
 })
 
-describe('createElements', () => {
-  test('no arguments', () => {
+describe("createElements", () => {
+  test("no arguments", () => {
     const result = createElements()
     expect(result.message).toBe(ERRORS.INSTANCE_NOT_DEFINED)
   })
 
-  test('valid instance', () => {
+  test("valid instance", () => {
     const mockFn = jest.fn()
     const instance = { elements: mockFn }
     createElements(instance)
     expect(mockFn).toHaveBeenCalledTimes(1)
   })
 
-  test('options passed', () => {
+  test("options passed", () => {
     const mockFn = jest.fn()
     const instance = { elements: mockFn }
-    const options = { a: 'b' }
+    const options = { a: "b" }
     createElements(instance, options)
     expect(mockFn).toHaveBeenCalledTimes(1)
     expect(mockFn).toHaveBeenCalledWith(options)
   })
 })
 
-describe('createElement', () => {
-  test('no arguments', () => {
+describe("createElement", () => {
+  test("no arguments", () => {
     const result = createElement()
     expect(result.message).toBe(ERRORS.ELEMENTS_NOT_DEFINED)
   })
 
-  test('valid elements, no type', () => {
+  test("valid elements, no type", () => {
     const elements = {}
     const result = createElement(elements)
     expect(result.message).toBe(ERRORS.ELEMENT_TYPE_NOT_DEFINED)
   })
 
-  test('valid elements, valid type', () => {
+  test("valid elements, valid type", () => {
     const mockFn = jest.fn()
     const elements = { create: mockFn }
-    const type = 'card'
+    const type = "card"
     createElement(elements, type)
     expect(mockFn).toHaveBeenCalledTimes(1)
   })
 
-  test('options passed', () => {
+  test("options passed", () => {
     const mockFn = jest.fn()
     const elements = { create: mockFn }
-    const type = 'card'
-    const options = { b: 'c' }
+    const type = "card"
+    const options = { b: "c" }
     createElement(elements, type, options)
     expect(mockFn).toHaveBeenCalledTimes(1)
     expect(mockFn).toHaveBeenCalledWith(type, options)
