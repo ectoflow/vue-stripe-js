5ee6ad32e4a830bc10284af7aa73379b6a4587a3
Oleksandr Bystrikov <coder@yakviter.com.ua>
2025-01-10 17:29:30 +0100

    fix: install stripe-js as dependency

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: ["vue"],
+      external: ["vue", "@stripe/stripe-js"],
       output: {
         // Provide global variables to use in the UMD build
         // for externalized deps
