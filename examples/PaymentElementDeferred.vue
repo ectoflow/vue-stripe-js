901878eb0a4e30493795be5bbceca7943293953e
Oleksandr Bystrikov <coder@yakviter.com.ua>
2025-01-15 11:18:08 +0100

    refactor(examples): style buttons and add v-if

--- examples/PaymentElementDeferred.vue
+++ examples/PaymentElementDeferred.vue
@@ -1,7 +1,6 @@
 <template>
-  <form @submit.prevent="handleSubmit">
+  <form v-if="stripeLoaded" @submit.prevent="handleSubmit">
     <StripeElements
-      v-if="stripeLoaded"
       :stripe-key="stripeKey"
       :instance-options="stripeOptions"
       :elements-options="elementsOptions"
@@ -16,7 +15,7 @@
     </StripeElements>
     <button
       type="submit"
-      class="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
+      class="focus:outline-none text-white bg-indigo-700 hover:bg-indigo-800 focus:ring-4 focus:ring-indigo-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-indigo-900"
     >
       Submit
     </button>
